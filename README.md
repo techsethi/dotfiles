07cf2313083addadc55c595201e0ca6010058ab4
Matt Johnson <grillpanda@me.com>
2012-04-03 15:11:04 +0100

    Goodbye zsh, you were interesting, but got in the way.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # dotfiles
 
 A bunch of config files.
-
-Includes my settings for bash (not getting much love)
-and zsh (current favourite child).
--- zsh/aliases.sh
+++
@@ -1,9 +0,0 @@
-# Global aliases
-
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-
-alias -g L='| less'
-alias -g S='| sort'
-alias -g G='| grep'
