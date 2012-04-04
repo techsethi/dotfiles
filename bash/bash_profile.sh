3e978de4629cbcb362df01d6fa70964969da4c01
Matt Johnson <grillpanda@me.com>
2012-04-04 14:22:51 +0100

    Set CDPATH and load bash-completion if we have it.

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -1,4 +1,9 @@
 export PATH="/usr/local/bin:$PATH"
+export CDPATH=".:..:~/_/songkick"
+
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  . `brew --prefix`/etc/bash_completion
+fi
 
 for file in ~/.dotfiles/bash/includes/*.sh; do
   [[ -r $file ]] && source $file;
