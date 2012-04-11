f33b6cdb9a474f7ba1aa03b6b906064cc7e6c374
Matt Johnson <grillpanda@me.com>
2012-04-11 08:41:26 +0100

    Bundle install to vendor/gems.

--- aliases/ruby.sh
+++ aliases/ruby.sh
@@ -2,5 +2,5 @@ alias ri='ri -Tf ansi'
 alias spec='spec -f n -c'
 
 alias be='bundle exec'
-alias biv='bundle install --path vendor'
+alias biv='bundle install --without production --path vendor/gems'
 alias bl='bundle list'
