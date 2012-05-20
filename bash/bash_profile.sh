afa78b7f64268e0669fce6ab617a9580dfd13531
Matt Johnson <grillpanda@me.com>
2012-05-20 16:38:15 +0100

    Add rbenv line to bash profile.

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -9,4 +9,6 @@ for file in ~/.dotfiles/bash/includes/*.sh; do
   [[ -r $file ]] && source $file;
 done
 
+eval "$(rbenv init -)"
+
 if [ -s ~/.bash_profile.local ]; then source ~/.bash_profile.local; fi
