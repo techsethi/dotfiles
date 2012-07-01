3d87c2f38f63f07fd8cd0b935d820e27b4bc6eea
Matt Johnson <grillpanda@me.com>
2012-07-01 11:25:42 +0100

    Add color theme, adapted from https://github.com/aziz/dotfiles.

---
+++ bash/includes/appearance.sh
@@ -0,0 +1,19 @@
+# colored grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;33'
+
+# colored ls
+export CLICOLOR=1
+export LSCOLORS='gxfxcxdxdxegedabagacad'
+
+# Less Colors for Man Pages from: http://bit.ly/uYa1mp
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+source ~/.dotfiles/bash/includes/theme/colours.sh
+source ~/.dotfiles/bash/includes/theme/current.sh
